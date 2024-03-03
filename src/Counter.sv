ee386880845ba3ffce47a6a1fa1180be42cf655e
Ibrahim1818 <muhammadibraheem23.mi@gmail.com>
2024-03-03 16:59:25 +0500

    Updated code according to verilog coding guidelines

--- src/Counter.sv
+++ src/Counter.sv
@@ -1,18 +1,22 @@
-module Counter(
-    input logic clk,           
-    input logic reset,         
-    input logic add_shift,
-    input logic shift,    
-    output logic [3:0] count,  
-    output logic count_check   
+module Counter #(
+  parameter WIDTH_C = 4
+) (
+  input logic                clk,           
+  input logic                reset,   
+
+  input logic                add_shift,
+  input logic                shift,    
+  
+  output logic [WIDTH_C-1:0] count,  
+  output logic               count_check   
 );
 
 always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        count <= 4'b0000;
-    end else if (add_shift || shift) begin
-        count <= (count == 15) ? 4'b0000 : count + 1;
-    end
+  if (reset) begin
+    count <= 4'b0000;
+  end else if (add_shift || shift) begin
+    count <= (count == 15) ? 4'b0000 : count + 1;
+  end
 end
 
 assign count_check = (count == 4'b1111);
--- src/Datapath.sv
+++ src/Datapath.sv
@@ -1,75 +1,93 @@
 // 180ns
-module Datapath(
-    input logic clk,           
-    input logic reset,         
-    input logic load_words,    
-    input logic add_shift, 
-    input logic shift,
-    input logic ready,
-    input logic [15:0] multiplier, 
-    input logic [15:0] multiplicand,
-    output logic [31:0] product,  
-    output logic count_check
+module Datapath #(
+  parameter WIDTH_M = 16,
+  parameter WIDTH_P = 32,
+  parameter WIDTH_C = 4
+) (
+  input logic                clk,           
+  input logic                reset,    
+
+  input logic                load_words,    
+  input logic                add_shift, 
+  input logic                shift,
+  input logic                ready,
+  input logic                flush,
+
+  input logic [WIDTH_M-1:0]  multiplier, 
+  input logic [WIDTH_M-1:0]  multiplicand,
+    
+  output logic [WIDTH_P-1:0] product,  
+  output logic               count_check,
+  output logic               empty
 );
 
-logic [3:0] count;
-logic [31:0] shifted_multiplicand;
-logic [15:0] shifted_multiplier;
-logic [31:0] alu_result;
+logic [WIDTH_C-1:0] count;
+logic [WIDTH_P-1:0] shifted_multiplicand;
+logic [WIDTH_M-1:0] shifted_multiplier;
+logic [WIDTH_P-1:0] alu_result;
+
+//Check if any of the multiplier or multiplicand is zero
+assign empty = (~|multiplier) || (~|multiplicand);
+
 
 Counter counter_inst (
-    .clk(clk),
-    .reset(reset),
-    .add_shift(add_shift),
-    .shift(shift),
-    .count(count),
-    .count_check(count_check)
+  .clk(clk),
+  .reset(reset),
+    
+  .add_shift(add_shift),
+  .shift(shift),
+    
+  .count(count),
+  .count_check(count_check)
 );
 
 ALU alu_inst (
-    .clk(clk),
-    .reset(reset),
-    .add_shift(add_shift),
-    .A(shifted_multiplicand),
-    .result(product)
+  .clk(clk),
+  .reset(reset),
+    
+  .flush(flush),  
+  .add_shift(add_shift),
+  .A(shifted_multiplicand),
+    
+  .result(product)
 );
 
 // Load Multiplicand
 always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        shifted_multiplicand <= 32'h00000000;
-    end else if (load_words) begin
-        shifted_multiplicand <= {16'b0,multiplicand};
-    end
+  if (reset) begin
+    shifted_multiplicand <= 32'h00000000;
+  end else if (load_words) begin
+    shifted_multiplicand <= {16'b0,multiplicand};
+  end
 end
 
 
 // Load Multiplier
 always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        shifted_multiplier <= 32'h00000000;
-    end else if (load_words) begin
-        shifted_multiplier <= multiplier;
-    end
+  if (reset) begin
+    shifted_multiplier <= 32'h00000000;
+  end else if (load_words) begin
+    shifted_multiplier <= multiplier;
+  end
 end
 
 // Left Shifter
 always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        shifted_multiplicand <= 32'h00000000;
-    end else if (shift || add_shift) begin
-        shifted_multiplicand <= shifted_multiplicand << 1;
-    end
+  if (reset) begin
+    shifted_multiplicand <= 32'h00000000;
+  end else if (shift || add_shift) begin
+    shifted_multiplicand <= shifted_multiplicand << 1;
+  end
 end
 
 // Right Shifter
 always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        shifted_multiplicand <= 32'h00000000;
-        shifted_multiplier <= 32'h00000000;
-    end else if (shift || add_shift) begin
-        shifted_multiplier <= shifted_multiplier >> 1;
-    end
+  if (reset) begin
+    shifted_multiplicand <= 32'h00000000;
+    shifted_multiplier   <= 32'h00000000;
+  end else if (shift || add_shift) begin
+    shifted_multiplier   <= shifted_multiplier >> 1;
+  end
 end
 
 endmodule
